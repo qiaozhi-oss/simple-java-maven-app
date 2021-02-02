1689008ef47ed4deb73876864295c0d2ae382056
jinguangshan <1192029998@qq.com>
2021-02-02 16:49:22 +0800

    20210202

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,28 +1,8 @@
 #!/usr/bin/env bash
-
-#echo 'The following Maven command installs your Maven-built Java application'
-#echo 'into the local Maven repository, which will ultimately be stored in'
-#echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-#echo 'volume).'
-#set -x
-#mvn jar:jar install:install help:evaluate -Dexpression=project.name
-#set +x
-
-#echo 'The following complex command extracts the value of the <name/> element'
-#echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-#set -x
+#摘取pom文件project.name,project.version
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
-echo ${NAME}
-#set +x
-
-#echo 'The following complex command behaves similarly to the previous one but'
-#echo 'extracts the value of the <version/> element within <project/> instead.'
-#set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-echo ${VERSION}
-#set +x
-
-#echo 'The following command runs and outputs the execution of your Java'
-#echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-#set -x
-#java -jar target/${NAME}-${VERSION}.jar
+#打印
+echo ${NAME}-${VERSION}
+#运行jar包
+java -jar ./target/${NAME}-${VERSION}.jar
