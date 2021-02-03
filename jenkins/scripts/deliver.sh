1ebb2cb547b6e911c12e7e0051b5ada4f9667806
jinguangshan <1192029998@qq.com>
2021-02-03 17:20:19 +0800

    20210203

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,5 +5,5 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 #打印
 echo "Path : ./target/${NAME}-${VERSION}.jar"
 #运行jar包
-BUILD_ID=DONTKILLME
-java -jar ./target/${NAME}-${VERSION}.jar
+BUILD_ID=dontKillMe
+nohup java -jar ./target/${NAME}-${VERSION}.jar &
