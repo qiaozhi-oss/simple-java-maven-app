a1c033d59bba5105ab1f90908a3d97de82dcc9c9
jinguangshan <1192029998@qq.com>
2021-02-03 16:35:21 +0800

    20210203

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #摘取pom文件project.name,project.version
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 #打印
 echo "Path : ./target/${NAME}-${VERSION}.jar"
