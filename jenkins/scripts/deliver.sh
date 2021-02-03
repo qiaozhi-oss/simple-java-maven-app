3e8feb5e8e34da03bf31c5d26e5b9a18c35b52b1
jinguangshan <1192029998@qq.com>
2021-02-03 17:57:34 +0800

    20210203

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -2,9 +2,25 @@
 #摘取pom文件project.name,project.version
 NAME=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-#打印
-echo "Path : ./target/${NAME}-${VERSION}.jar"
+
+#打印jar包路径日志
+echo "JAR Path : ./target/${NAME}-${VERSION}.jar"
+
+#杀死已存在进程
+# if [ -n "$xxx" ]用于判断xxx变量非空
+# fi为if语句的结束,相当于end if
+pid=`ps -ef | grep ${NAME}-${VERSION}.jar|grep -v grep|awk '{print $2}'`
+if [ -n "$pid" ]
+then
+  kill -9 $pid
+  echo "${NAME}-${VERSION}.jar Killed"
+fi
+
 #运行jar包
+#设定BUILD_ID防止被jenkins杀掉
 BUILD_ID=dontKillMe
+#后台jar包启动
 nohup java -jar ./target/${NAME}-${VERSION}.jar &
+
+#打印启动成功日志
 echo "${NAME}-${VERSION}.jar Start successful"
