44b97165d11c3ffe126b35747f25e2bafb24dd04
jinguangshan <1192029998@qq.com>
2021-02-04 09:37:27 +0800

    20210203

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -18,6 +18,7 @@ fi
 
 #运行jar包
 #设定BUILD_ID防止被jenkins杀掉
+echo $BUILD_ID
 BUILD_ID=1234
 #后台jar包启动,并将日志输出到application.log 文件
 nohup java -Xms800m -Xmx800m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m -XX:MaxNewSize=512m -jar ./target/${NAME}-${VERSION}.jar 1>/var/lib/jenkins/workspace/application.log 2>&1 &
